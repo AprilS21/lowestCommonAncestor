0d06767c93b55cfe04ba881d1bb6f86204c69eaf
AprilS21 <asheeran@tcd.ie>
2021-10-04 11:43:07 +0100

    added error checking to user input

--- src/LCA.java
+++ src/LCA.java
@@ -1,18 +1,20 @@
 import java.util.Scanner;
 
-import org.junit.Test;
 
 public class LCA {
    
     public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     BST bst = new BST();
-    System.out.println("Type 'add' to add values to you bst or 'lca' to find the lowest ancestor");
+    System.out.println("Add values to you bst or 'lca' to find the lowest ancestor");
     int i=0;
     boolean done = false;
     while(done==false && scanner.hasNext()){
     String add = scanner.nextLine();
-    if(!add.equals("lca")){
+    if(!add.equals("lca") && add.length()>1){
+        System.out.println("Please enter only one character/digit");
+    }
+    else if(!add.equals("lca")){
         bst.put(add,i);
         i++;
         System.out.println(add +" added to bst. Type another value to add or 'lca'");
